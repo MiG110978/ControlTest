905b0f996cd9f97503b472d908a9b0fe0406fa64
Mikhail Gorbatyuk <MikeGor2001@mail.ru>
2022-02-20 00:07:00 +0300

    sixth commit - added comments

--- Program.cs
+++ Program.cs
@@ -22,19 +22,19 @@ try
     Console.WriteLine("Заполниет исходный массив со строковыми элементами");
     Console.Write("Введите количество элементов исходного массива - ");
     int size = int.Parse(Console.ReadLine());
-    string[] arrayString = new string[size];
-    FillingArrayString(arrayString);
+    string[] arrayString = new string[size]; // исходный массив
+    FillingArrayString(arrayString); //заполнение исходного массива
     Console.Write("Проверьте, вы ввели массив - \t \t");
     PrintArrayString(arrayString);
-    string[] arrayStringNew = new string[0];
+    string[] arrayStringNew = new string[0]; // создание  нового выходного пустого массива 
     int sizeNew = 0;
-    int maxNumberSymbol = 3;
-    foreach (var element in arrayString)
+    int maxNumberSymbol = 3; // максимальное колличество символов строк, которые будут попадать в новый массив
+    foreach (var element in arrayString) // проходим по всем элементам исходного массива
     {
-        if (element.Length <= maxNumberSymbol)
+        if (element.Length <= maxNumberSymbol) // проверяем длину каждого элемента с максимальным количеством символов
         {
-            Array.Resize(ref arrayStringNew, sizeNew + 1);
-            arrayStringNew[sizeNew] = element;
+            Array.Resize(ref arrayStringNew, sizeNew + 1); // если элемент подходит для нового массива, то увеличиваем длину этого нового массива на 1
+            arrayStringNew[sizeNew] = element; // вносим подходящий элемент в новый массив
             sizeNew++;
         }
     }
